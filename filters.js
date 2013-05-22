a8b91c5302215aaa91f5e95239eb116120fbde0b
Yehor Lvivski <lvivski@gmail.com>
2013-05-22 16:31:37 +0300

    initial pages

--- filters.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest()
-xhr.open('GET', chrome.extension.getURL("filters.svg"))
-xhr.addEventListener('load', function(e) {
-  document.body.appendChild(xhr.responseXML.documentElement)
-})
-xhr.send()
