8e1939cac51d2ba18855211c91e9b5ef3979cada
Yehor Lvivski <lvivski@gmail.com>
2017-01-11 19:08:49 +0200

    1.1.6 remove on normal

--- chrome/js/filters.js
+++ chrome/js/filters.js
@@ -1,7 +1,7 @@
-var filter = null;
-var xhr = new XMLHttpRequest()
+let filter = null
+const xhr = new XMLHttpRequest()
 xhr.open('GET', chrome.extension.getURL('img/filters.svg'))
-xhr.addEventListener('load', function(e) {
+xhr.addEventListener('load', (e) => {
   filter = xhr.responseXML.documentElement
   filter.style.display = 'none'
   filter.width = 0
@@ -9,15 +9,18 @@ xhr.addEventListener('load', function(e) {
 })
 xhr.send()
 
-function applyFilter(){
-    document.body.appendChild(filter);
-}
+sessionStorage.removeItem('spectrumFilter');
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendReponse){
-    if (request.method=="load"){
-      applyFilter();
-      sendResponse(true);
-    }
-  }
-)
+chrome.runtime.onMessage.addListener((message, sender, sendReponse) => {
+	if (message.type === 'getFilter') {
+		sendReponse(sessionStorage.getItem('spectrumFilter'))
+	} else if (message.type === 'applyFilter') {
+		sessionStorage.setItem('spectrumFilter', message.filter);
+		if (message.filter !== 'normal' && filter.parentNode !== document.body) {
+			document.body.appendChild(filter)
+		} else if (message.filter === 'normal') {
+			filter.parentNode.removeChild(filter)
+		}
+		sendReponse()
+	}
+})
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -1,52 +1,60 @@
 function $(selector, context) {
   return (context || document).querySelector(selector)
 }
- 
+
 $.all = function (selector, context) {
   return Array.prototype.slice.call(
     (context || document).querySelectorAll(selector)
   )
 }
 
-var ul = document.createElement('ul'),
-    current = 'normal',
-    vision = {
-      normal: '',
-      protanopia: '1%',
-      protanomaly: '1%',
-      deuteranopia: '1%',
-      deuteranomaly: '6%',
-      tritanopia: '0.1%',
-      tritanomaly: '0.01%',
-      achromatopsia: '0.00001%',
-      achromatomaly: '0.00001%',
-      'low-contrast': ''
-    }
-
-Object.keys(vision).forEach(function (el) {
-  var li = document.createElement('li')
-  li.dataset['type'] = el
-  li.textContent = el
-  li.addEventListener('click', handler, false)
-  el == current && li.classList.add('current')
+const ul = document.createElement('ul')
+const vision = {
+	normal: '',
+	protanopia: '1%',
+	protanomaly: '1%',
+	deuteranopia: '1%',
+	deuteranomaly: '6%',
+	tritanopia: '0.1%',
+	tritanomaly: '0.01%',
+	achromatopsia: '0.00001%',
+	achromatomaly: '0.00001%',
+	'low-contrast': ''
+}
+Object.keys(vision).forEach((type) => {
+	const li = document.createElement('li')
+  li.dataset.type = type
+  li.textContent = type
+	li.addEventListener('click', handler, false)
   ul.appendChild(li)
+})
+
+update('normal')
+chrome.tabs.query({ active:true, currentWindow:true }, (tabs) => {
+	chrome.tabs.sendMessage(tabs[0].id, { type: 'getFilter' }, (response) => {
+		update(response || 'normal')
+	})
 })
 
 document.body.appendChild(ul)
 
-var filterLoaded = false;
+function update(type) {
+	$.all('li').forEach(li => {
+		if (li.dataset.type === type) {
+			li.classList.add('current')
+		} else {
+			li.classList.remove('current')
+		}
+	})
+}
+
 function handler(e) {
-  if (!filterLoaded){
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id, {method:"load"}, function(response){
-        filterLoaded = response;
-      });
-    });
-  }
-  current = this.dataset['type']
-  $.all('li').forEach(function(li) {
-    li.classList.remove('current')
-  })
-  this.classList.add('current')
-  chrome.tabs.insertCSS(null, { code: 'html { -webkit-filter: url(#' + current + '); }' })
+	const type = this.dataset.type
+	update(type)
+  chrome.tabs.query({ active:true, currentWindow:true }, (tabs) => {
+		const tab = tabs[0].id;
+		chrome.tabs.sendMessage(tab, { type: 'applyFilter', filter: type }, (message) => {
+			chrome.tabs.insertCSS(tab, { code: `html { -webkit-filter: url(#${type}); }` })
+		})
+	})
 }
