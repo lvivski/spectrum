d02184358acca58abeceea95fe0bc8bc1636e931
Nathan Jervis <nathanj@airongroup.ca>
2017-01-11 09:46:53 -0500

    Lazily attach the svg filter to the page
    the content script loads the svg but doesn't attach it until the extension window tells it to. When you select a colour profile for the first time the extension will tell the content script to load.
    
    This prevents the extension from interfering with pages until actually used. This fixes the known issue with bitbucket in #3 and prevents further issues.

--- chrome/js/filters.js
+++ chrome/js/filters.js
@@ -1,10 +1,23 @@
+var filter = null;
 var xhr = new XMLHttpRequest()
 xhr.open('GET', chrome.extension.getURL('img/filters.svg'))
 xhr.addEventListener('load', function(e) {
-  var filter = xhr.responseXML.documentElement
+  filter = xhr.responseXML.documentElement
   filter.style.display = 'none'
   filter.width = 0
   filter.height = 0
-  document.body.appendChild(filter)
 })
 xhr.send()
+
+function applyFilter(){
+    document.body.appendChild(filter);
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendReponse){
+    if (request.method=="load"){
+      applyFilter();
+      sendResponse(true);
+    }
+  }
+)
