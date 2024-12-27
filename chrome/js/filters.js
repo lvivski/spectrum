4b09a620ccea9b8fdf3b5bc8985c15702c329ed7
Yehor Lvivski <yehor@lvivski.com>
2024-12-26 16:40:55 -0800

    update

--- chrome/js/filters.js
+++ chrome/js/filters.js
@@ -1,28 +1,28 @@
-let svg = null
-const xhr = new XMLHttpRequest()
-xhr.open('GET', chrome.extension.getURL('img/filters.svg'))
-xhr.addEventListener('load', (e) => {
-	svg = xhr.responseXML.documentElement
-	svg.style.display = 'none'
-	svg.width = 0
-	svg.height = 0
-})
-xhr.send()
+let svg = null;
+const xhr = new XMLHttpRequest();
+xhr.open("GET", chrome.extension.getURL("img/filters.svg"));
+xhr.addEventListener("load", (e) => {
+  svg = xhr.responseXML.documentElement;
+  svg.style.display = "none";
+  svg.width = 0;
+  svg.height = 0;
+});
+xhr.send();
 
-sessionStorage.removeItem('spectrumFilter')
+sessionStorage.removeItem("spectrumFilter");
 
-chrome.runtime.onMessage.addListener((message, sender, sendReponse) => {
-	let filter = 'normal'
-	if (message.type === 'restoreFilter') {
-		filter = sessionStorage.getItem('spectrumFilter')
-	} else if (message.type === 'applyFilter') {
-		filter = message.filter
-		sessionStorage.setItem('spectrumFilter', message.filter)
-		if (filter !== 'normal' && svg.parentNode !== document.body) {
-			document.body.appendChild(svg)
-		} else if (filter === 'normal') {
-			svg.parentNode.removeChild(svg)
-		}
-	}
-	sendReponse(filter)
-})
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let filter = "normal";
+  if (message.type === "restoreFilter") {
+    filter = sessionStorage.getItem("spectrumFilter");
+  } else if (message.type === "applyFilter") {
+    filter = message.filter;
+    sessionStorage.setItem("spectrumFilter", message.filter);
+    if (filter !== "normal" && svg.parentNode !== document.body) {
+      document.body.appendChild(svg);
+    } else if (filter === "normal") {
+      svg.parentNode.removeChild(svg);
+    }
+  }
+  sendResponse(filter);
+});
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -1,56 +1,76 @@
-const $ = (selector, context) => (context || document).querySelector(selector)
+const $ = (selector, context) => (context || document).querySelector(selector);
 
-$.all = (selector, context) => Array.prototype.slice.call(
-	(context || document).querySelectorAll(selector)
-)
+$.all = (selector, context) =>
+  Array.prototype.slice.call((context || document).querySelectorAll(selector));
 
-const ul = document.createElement('ul')
+const ul = document.createElement("ul");
 const vision = {
-	normal: '',
-	protanopia: '1%',
-	protanomaly: '1%',
-	deuteranopia: '1%',
-	deuteranomaly: '6%',
-	tritanopia: '0.1%',
-	tritanomaly: '0.01%',
-	achromatopsia: '0.00001%',
-	achromatomaly: '0.00001%',
-	'low-contrast': ''
-}
+  normal: "",
+  protanopia: "1%",
+  protanomaly: "1%",
+  deuteranopia: "1%",
+  deuteranomaly: "6%",
+  tritanopia: "0.1%",
+  tritanomaly: "0.01%",
+  achromatopsia: "0.00001%",
+  achromatomaly: "0.00001%",
+  "low-contrast": "",
+};
 Object.keys(vision).forEach((type) => {
-	const li = document.createElement('li')
-	li.dataset.type = type
-	li.textContent = type
-	li.addEventListener('click', handler, false)
-	ul.appendChild(li)
-})
+  const li = document.createElement("li");
+  li.dataset.type = type;
+  li.textContent = type;
+  li.addEventListener("click", handler, false);
+  ul.appendChild(li);
+});
 
-chrome.tabs.query({ active:true, currentWindow:true }, (tabs) => {
-	const tab = tabs[0].id;
-	chrome.tabs.sendMessage(tab, { type: 'restoreFilter' }, (filter) => {
-		update(filter)
-	})
-})
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tabId = tabs[0].id;
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      function: restoreFilter,
+    },
+    (results) => {
+      const filter = results[0].result;
+      update(filter);
+    }
+  );
+});
 
-document.body.appendChild(ul)
+document.body.appendChild(ul);
 
 function update(type) {
-	$.all('li').forEach(li => {
-		if (li.dataset.type === type) {
-			li.classList.add('current')
-		} else {
-			li.classList.remove('current')
-		}
-	})
+  $.all("li").forEach((li) => {
+    if (li.dataset.type === type) {
+      li.classList.add("active");
+    } else {
+      li.classList.remove("active");
+    }
+  });
+}
+
+function restoreFilter() {
+  return new Promise((resolve) => {
+    chrome.runtime.sendMessage({ type: "restoreFilter" }, (response) => {
+      resolve(response.filter);
+    });
+  });
 }
 
 function handler(e) {
-	const filter = this.dataset.type
-	update(filter)
-	chrome.tabs.query({ active:true, currentWindow:true }, (tabs) => {
-		const tab = tabs[0].id;
-		chrome.tabs.sendMessage(tab, { type: 'applyFilter', filter: filter }, () => {
-			chrome.tabs.insertCSS(tab, { code: `body { -webkit-filter: url(#${filter}); }` })
-		})
-	})
+  const filter = this.dataset.type;
+  update(filter);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0].id;
+    chrome.tabs.sendMessage(
+      tab,
+      { type: "applyFilter", filter: filter },
+      () => {
+        chrome.tabs.insertCSS(tab, {
+          code: `body { -webkit-filter: url(#${filter}); }`,
+        });
+      }
+    );
+  });
 }
