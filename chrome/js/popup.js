550a98791996d0cc0fb18b6d6e76f4e311cddd64
Yehor Lvivski <yehor@lvivski.com>
2024-12-27 16:39:34 -0800

    add comments back

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -2,6 +2,17 @@ const $ = (selector, context) => (context || document).querySelector(selector);
 const $$ = (selector, context) =>
   Array.prototype.slice.call((context || document).querySelectorAll(selector));
 
+/*
+FYI http://en.wikipedia.org/wiki/Color_blindness
+further reading:
+http://www.inf.ufrgs.br/~oliveira/pubs_files/CVD_Simulation/CVD_Simulation.html
+http://www.inf.ufrgs.br/~oliveira/pubs_files/CVD_PCA/CVD_PCA.html
+http://vision.psychol.cam.ac.uk/jdmollon/papers/Dichromatsimulation.pdf
+http://vision.psychol.cam.ac.uk/jdmollon/papers/colourmaps.pdf
+http://scien.stanford.edu/class/psych221/projects/05/ofidaner/project_report.pdf
+http://scien.stanford.edu/pages/labsite/2005/psych221/projects/05/ofidaner/conv_img.m
+Note: use `web.archive.org` for old links
+*/
 const filters = {
   normal: `<filter id="normal">
 			<feColorMatrix values="1 0 0 0 0
